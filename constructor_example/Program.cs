e3533e6a8c42b508a55668b10af61fbcb112abf2
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-12 19:19:09 +0300

    Proje dosyası ekle.

---
+++ constructor_example/Program.cs
@@ -0,0 +1,28 @@
+﻿using constructor_example;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Default Constructor ile bebek nesnesi oluşturma
+        baby baby1 = new baby();
+
+        baby1.name = "ahsen";
+        baby1.surname = "durmaz";
+
+        baby1.PrintInformation();
+
+        //alternative
+        
+        baby baby2 = new baby();
+        baby2.name = "metehan";
+        baby2.surname = "karahan";
+
+        baby2.PrintInformation();
+
+
+    }
+    
+}
---
+++ constructor_example/baby.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_example
+{
+    public class baby
+    {
+
+        //property tanımlamaları
+        public DateTime dateofbirth { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        //default
+        public baby()
+            {
+            dateofbirth =  DateTime.Now;
+            Console.WriteLine("ıngaaaa");
+
+
+            }
+        //alternative 
+
+        public baby(string name, string surname)
+        {
+
+            name = name;
+            surname = surname;
+            dateofbirth = DateTime.Now;
+
+            Console.WriteLine($"ıngaaaaaa");
+
+
+        }
+
+        //method to print baby information
+
+        public void PrintInformation()
+        {
+            Console.WriteLine($"name: {name} surname: {surname} date of birth: {dateofbirth} ");
+
+        }
+    }
+}
